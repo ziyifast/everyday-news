8b2d0c6878b70d1b6b49752f27dcdf82ce30e400
zhouyi <ali79597@163.com>
2024-04-21 19:24:32 +0800

    add upload file api

---
+++ src/main/java/com/ziyi/controller/FileController.java
@@ -0,0 +1,28 @@
+package com.ziyi.controller;
+
+import com.ziyi.pojo.Result;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author zhouYi
+ * @description TODO
+ * @date 2024/4/21 19:19
+ */
+@RestController
+public class FileController {
+
+    // 上传文件
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        String filename = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
+        file.transferTo(new File("/Users/ziyi2/Desktop/local-pro/files/" + filename));
+        return Result.success("可访问的URL地址...");
+    }
+}
