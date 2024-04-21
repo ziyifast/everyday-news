8683483113c8b33af5251f020701a1ea09476e89
zhouyi <ali79597@163.com>
2024-04-21 10:07:58 +0800

    init pro

---
+++ src/main/java/com/ziyi/BigEventApplication.java
@@ -0,0 +1,16 @@
+package com.ziyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouYi
+ * @description TODO
+ * @date 2024/4/21 09:35
+ */
+@SpringBootApplication
+public class BigEventApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BigEventApplication.class, args);
+    }
+}
