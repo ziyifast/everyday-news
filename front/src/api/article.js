112ab24c55a92b79813936cfe54409aac6fa3e69
zhouyi <ali79597@163.com>
2024-04-24 22:05:30 +0800

    [front] add category & update category

--- front/src/api/article.js
+++ front/src/api/article.js
@@ -4,8 +4,18 @@ import { useTokenStore } from '@/store/token.js'
 // 获取文章分类
 export const articleCategoryListService = () => {
     //请求头携带token
-    const tokenStore = useTokenStore()
+    // const tokenStore = useTokenStore()
     // //pinia创建的响应式对象不用.value，因此直接使用tokenStore.token=》改为请求拦截器添加token
     // console.log('token=',tokenStore.token)
     return request.get('/category/list')
+}
+
+//添加文章分类
+export const addArticleCategoryService = (articleCategory) => {
+    return request.post('/category/add', articleCategory)
+}
+
+//修改文章分类
+export const updateArticleCategoryService = (articleCategory) => {
+    return request.put('/category/update', articleCategory)
 }
--- front/src/views/article/ArticleCategory.vue
+++ front/src/views/article/ArticleCategory.vue
@@ -3,9 +3,9 @@ import {
     Edit,
     Delete
 } from '@element-plus/icons-vue'
-import {ElMessage} from 'element-plus'
+import { ElMessage } from 'element-plus'
 import { ref } from 'vue'
-import { articleCategoryListService } from '@/api/article.js'
+import { articleCategoryListService,addArticleCategoryService,updateArticleCategoryService } from '@/api/article.js'
 //获取文章分类
 const getArticleCategoryList = async () => {
     let result = await articleCategoryListService()
@@ -22,102 +22,117 @@ getArticleCategoryList()
 const categorys = ref([
 
 ])
+
+const title = ref('')
+//控制添加分类弹窗
+const dialogVisible = ref(false)
+
+//添加分类数据模型
+const categoryModel = ref({
+    id:0,
+    categoryName: '',
+    categoryAlias: ''
+})
+//清空模型数据
+const clearCategoryModel = ()=>{
+    categoryModel.value.categoryName='',
+    categoryModel.value.categoryAlias=''
+}
+//添加分类表单校验
+const rules = {
+    categoryName: [
+        { required: true, message: '请输入分类名称', trigger: 'blur' },
+    ],
+    categoryAlias: [
+        { required: true, message: '请输入分类别名', trigger: 'blur' },
+    ]
+}
+//回显数据
+const updateCategoryEcho = (row) => {
+    categoryModel.value.id = row.id
+    categoryModel.value.categoryName = row.categoryName
+    categoryModel.value.categoryAlias = row.categoryAlias
+}
+
+//修改文章分类
+const updateCategory = async () => {
+    let result = await updateArticleCategoryService(categoryModel.value)
+    if (result.code === 0) {
+        ElMessage.success('修改成功')
+        dialogVisible.value = false
+        getArticleCategoryList()
+        return
+    }
+    ElMessage.error(result.message ? '' : '服务异常')
+    return
+}
+
+//添加文章分类
+const addArticleCategory = async () => {
+    addArticleCategory.value=''
+    let result = await addArticleCategoryService(categoryModel.value)
+    if (result.code === 0) {
+        ElMessage.success('添加成功')
+        dialogVisible.value = false
+        getArticleCategoryList()
+        categoryModel.value = ''
+        return
+    }
+    ElMessage.error(result.message ? '' : '服务异常')
+    return
+}
+ 
+//控制弹窗
 </script>
 <template>
     <el-card class="page-container">
-    
-    
-    
-    
-    
-    
-    
         <template #header>
-    
-        
-    
-            
-    
-        
-    
-                        <div class="header">
-    
-        
-    
-            
-    
-        
-    
-                            <span>文章分类</span>
-    
-        
-    
-            
-    
-        
-    
-                            <div class="extra">
-    
-        
-    
-            
-    
-        
-    
-                                <el-button type="primary">添加分类</el-button>
-    
-        
-    
-            
-    
-        
-    
-                            </div>
-    
-        
-    
-            
-    
-        
-    
-                        </div>
-</template>
+            <div class="header">
+                <span>文章分类</span>
+                <div class="extra">
+                    <el-button type="primary" @click=";title='添加分类';dialogVisible = true;clearCategoryModel()">添加分类</el-button>
+                </div>
+            </div>
+        </template>
         <el-table :data="categorys" style="width: 100%">
             <el-table-column label="序号" width="100" type="index"> </el-table-column>
             <el-table-column label="分类名称" prop="categoryName"></el-table-column>
             <el-table-column label="分类别名" prop="categoryAlias"></el-table-column>
             <el-table-column label="操作" width="100">
-<template #default="{ row }">
-    <el-button :icon="Edit" circle plain type="primary">
-    
-    
-    
-    
-    
-    
-    
-    </el-button>
-    
-    
-    
-    
-    
-    
-    
-    <el-button :icon="Delete" circle plain type="danger"></el-button>
-</template>
+                <template #default="{ row }">
+                    <el-button :icon="Edit" circle plain type="primary" @click="title='修改分类';updateCategoryEcho(row);dialogVisible = true;" ></el-button>
+                    <el-button :icon="Delete" circle plain type="danger"></el-button>
+                </template>
             </el-table-column>
-<template #empty>
-    <el-empty description="没有数据" />
-</template>
+            <template #empty>
+                <el-empty description="没有数据" />
+            </template>
         </el-table>
     </el-card>
+    <!-- 添加分类弹窗 -->
+<el-dialog v-model="dialogVisible" :title="title" width="30%">
+    <el-form :model="categoryModel" :rules="rules" label-width="100px" style="padding-right: 30px">
+        <el-form-item label="分类名称" prop="categoryName">
+            <el-input v-model="categoryModel.categoryName" minlength="1" maxlength="10"></el-input>
+        </el-form-item>
+        <el-form-item label="分类别名" prop="categoryAlias">
+            <el-input v-model="categoryModel.categoryAlias" minlength="1" maxlength="15"></el-input>
+        </el-form-item>
+    </el-form>
+    <template #footer>
+        <span class="dialog-footer">
+            <el-button @click="dialogVisible = false;">取消</el-button>
+            <el-button type="primary" @click="title==='添加分类'?addArticleCategory():updateCategory();"> 确认 </el-button>
+        </span>
+    </template>
+</el-dialog>
 </template>
 
 <style lang="scss" scoped>
 .page-container {
     min-height: 100%;
     box-sizing: border-box;
+
     .header {
         display: flex;
         align-items: center;
